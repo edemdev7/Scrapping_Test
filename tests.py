396086db4f20a41345bacdf015232ca00e7da8cd
edemdev7 <ekpomachi@gmail.com>
2024-05-16 19:12:11 +0200

    push

--- tests.py
+++ tests.py
@@ -5,7 +5,6 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Embedding
-from sklearn.metrics import accuracy_score
 
 # Charger les données à partir du fichier CSV
 data = pd.read_csv("all-data.csv")
@@ -39,48 +38,9 @@ model = Sequential([
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Entraîner le modèle
-history = model.fit(X_train_padded, y_train, epochs=10, batch_size=32)
+model.fit(X_train_padded, y_train, epochs=10, batch_size=32)
 
 # Évaluer le modèle sur les données de test
 loss, accuracy = model.evaluate(X_test_padded, y_test)
 print("Loss:", loss)
 print("Accuracy:", accuracy)
-
-# Enregistrer les prédictions du modèle sur les données de test dans un DataFrame
-y_pred = model.predict(X_test_padded)
-predictions = pd.DataFrame({'True Sentiment': y_test, 'Predicted Sentiment': y_pred.flatten()})
-
-
-
-# Calculer les statistiques pour chaque type de sentiment
-sentiments = label_encoder.inverse_transform(data['sentiment'].unique())
-statistics = []
-
-for sentiment in sentiments:
-    true_sentiments = predictions[predictions['True Sentiment'] == sentiment]
-    total_examples = len(true_sentiments)
-    if total_examples == 0:
-        accuracy = None
-    else:
-        accuracy = accuracy_score(true_sentiments['True Sentiment'], true_sentiments['Predicted Sentiment'])
-    loss = history.history['loss'][-1]  # Utiliser la dernière perte comme perte moyenne pour chaque sentiment
-    statistics.append({
-        'Sentiment': sentiment,
-        'Total Examples': total_examples,
-        'Accuracy': accuracy,
-        'Loss': loss
-    })
-
-# Créer un DataFrame pandas à partir des statistiques
-statistics_df = pd.DataFrame(statistics)
-
-# Enregistrer le DataFrame dans un fichier CSV
-statistics_df.to_csv('sentiment_statistics.csv', index=False)
-
-
-
-# Créer un DataFrame pandas pour les statistiques
-df_statistics = pd.DataFrame(statistics)
-
-# Enregistrer les résultats de l'évaluation du modèle et les statistiques dans un fichier CSV
-df_statistics.to_csv('sentiment_statistics.csv', index=False)
